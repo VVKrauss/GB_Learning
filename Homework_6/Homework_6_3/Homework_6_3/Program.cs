6e56912ee0873732e018f2eb5ca68a0ad7b77fa5
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2022-10-14 23:48:42 +0700

    Домашка 6 - закончена

--- Homework_6/Homework_6_3/Homework_6_3/Program.cs
+++ Homework_6/Homework_6_3/Homework_6_3/Program.cs
@@ -19,78 +19,73 @@ namespace Homework_6_3
         public static byte[] BytesArray(string fileName)
         {
 
-            byte[] bytes = new byte[1024];
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BufferedStream bufferedStream = new BufferedStream(fileStream);
 
+            byte[] bytes = new byte[bufferedStream.Length];
 
+            bufferedStream.Read(bytes, 0, bytes.Length);
+            bufferedStream.Close();
+            fileStream.Close();
 
-
-            return null; //TODO - replace
+            return bytes; 
         }
 
-        //public static string[] StringArray(string fileName)
-        //{
-        //    FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-        //    StreamReader streamReader = new StreamReader(fileStream);
+        public static string[] StringArray(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStream);
 
-        //    List<string> stringsCollection = new List<string>();
+            List<string> stringsCollection = new List<string>();
 
-        //    while (!streamReader.EndOfStream) {
-        //        stringsCollection.Add(streamReader.ReadLine());
-        //    }
+            while (!streamReader.EndOfStream)
+            {
+                stringsCollection.Add(streamReader.ReadLine());
+            }
 
-        //    string[] lines = new string[stringsCollection.Count];
+            string[] lines = new string[stringsCollection.Count];
 
-        //    int i = 0;
-        //    foreach (var line in stringsCollection)
-        //    {
-        //        lines[i] = line;
-        //        i++;
-        //    }
+            int i = 0;
+            foreach (var line in stringsCollection)
+            {
+                lines[i] = line;
+                i++;
+            }
 
-        //    streamReader.Close();
-        //    fileStream.Close();
+            streamReader.Close();
+            fileStream.Close();
 
-        //    return lines; 
-        //}
+            return lines;
+        }
 
-        public static void BinaryArray(string fileName)
+        public static int[] BinaryArray(string fileName)
         {
             FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             BinaryReader binaryReader = new BinaryReader(fileStream);
 
-            List<int> intCollection = new List<int>();
+            List<char> charCollection = new List<char>();
 
             int count = (int)fileStream.Length;
-            Console.WriteLine($"байт {count}");
 
-            for (int i = 0; i < count - 1; i++)
+            for (int i = 0; i < count; i++)
             {
-
-                int intValue = (int) binaryReader.ReadByte();
-
-                intCollection.Add(intValue);
-
-                Console.WriteLine($"байт {intValue}");
+                char byteValue = binaryReader.ReadChar();
+                charCollection.Add(byteValue);
             }
 
+            binaryReader.Close();
+            fileStream.Close();
 
-            //int[] lines = new int[count];
+            int[] result = new int[charCollection.Count];
 
+            int counter = 0;
+            foreach (char elemant in charCollection)
+            {
+                result[counter] = int.Parse(elemant.ToString());
+                counter++;  
+            }
 
-            //for (int i = 0; i < count; i++)
-            //{
-            //    lines[i] = (int) binaryReader.ReadSingle();
-
-            //foreach (var line in lines) 
-            //    Console.WriteLine($"байт {lines}");
-
-            //}
-
-
-
-
-
-            //   return lines;
+            return result;
         }
 
         static void Main(string[] args)
@@ -100,29 +95,27 @@ namespace Homework_6_3
 
             Krauss.Utilities.Helper.PrintInfo(6, 3);
 
-            string fileName1 = "pushkin.txt";
+            string fileName1 = "drizer.txt";
             string fileName2 = "dostoevskiy.txt";
-            string fileName3 = "chislafibonachi.txt";
-
+            string fileName3 = "chisla.txt";
 
             byte[] bytes = BytesArray(fileName1);
-            // string[] strings = StringArray(fileName2);
-            //int[] ints = 
-            BinaryArray(fileName3);
+            string[] strings = StringArray(fileName2);
+
+            int[] intArray = BinaryArray(fileName3);
 
 
-            //Console.WriteLine($"\nВыводим содержимое файла {fileName1}");
-            //foreach (var byteElement in bytes)
-            //    Console.WriteLine(byteElement);
+            Console.WriteLine($"\nВыводим содержимое файла {fileName1} \n");
+            foreach (var byteElement in bytes)
+                Console.Write(((char)byteElement).ToString());
 
-            //Console.WriteLine($"\nВыводим содержимое файла {fileName2}");
-            //foreach (var stringElement in strings)
-            //    Console.WriteLine(stringElement);
+            Console.WriteLine($"\nВыводим содержимое файла {fileName2} \n");
+            foreach (var stringElement in strings)
+                Console.WriteLine(stringElement);
 
-            Console.WriteLine($"\nВыводим содержимое файла {fileName3}");
-            foreach (var intElement in bytes)
-                Console.WriteLine(intElement);
-
+            Console.WriteLine($"\nВыводим содержимое файла {fileName3} \n");
+            foreach (var intElement in intArray)
+                Console.Write(intElement+"\t");
 
             Krauss.Utilities.Helper.EndProgramm();
 
